f39dabcfa14e52ff96cf95f36ba8e4c0a3a807bc
Inupa Udara <127815279+inupaUdara@users.noreply.github.com>
2025-04-21 15:57:35 +0530

    fix: fix user id bug

--- server/order-purchasing-service/index.js
+++ server/order-purchasing-service/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use("/api/v1/orders", orderRoutes);
 
-connectRabbitMQ();
+// connectRabbitMQ();
 
 app.listen(PORT, async () => {
   console.log(`Order service running on port ${PORT}`);
--- server/order-purchasing-service/src/routes/order.routes.js
+++ server/order-purchasing-service/src/routes/order.routes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/", authenticateToken, createOrder);
 router.get("/my-orders", authenticateToken, getUserOrders);
 router.get("/:orderId", authenticateToken, getOrderById);
-router.patch("/:orderId/status", authenticateToken, updateOrderStatus);
+router.patch("/:orderId", authenticateToken, updateOrderStatus);
 router.delete("/:orderId", authenticateToken, cancelOrder);
 
 module.exports = router;
