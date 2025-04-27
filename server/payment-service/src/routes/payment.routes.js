5283d9a46fef6c3fb6a2fe2d64e0e30fd3598364
sachithnim <nimendras07@gmail.com>
2025-04-27 12:01:00 +0530

    fix: uncomment authentication middleware for create-payment route

--- server/payment-service/src/routes/payment.routes.js
+++ server/payment-service/src/routes/payment.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { createPayment } = require('../controllers/payment.controller');
+const { authenticateToken } = require('../middleware/auth.middleware');
 
 
 const router = express.Router();
 
 
+// router.post('/create-payment',authenticateToken, createPayment);
 router.post('/create-payment', createPayment);
 
 
