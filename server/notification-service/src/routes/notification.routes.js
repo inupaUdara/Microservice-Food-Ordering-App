581c545502f06a1c95e0f593664fe8b57143792d
sachithnim <nimendras07@gmail.com>
2025-04-29 04:24:34 +0530

    feat: add cancelOrder route to notification service

--- server/notification-service/src/routes/notification.routes.js
+++ server/notification-service/src/routes/notification.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { confirmOrder, confirmRegistration, confirmOrderCompletion } = require('../controllers/confirmation.controller');
+const { confirmOrder, confirmRegistration, confirmOrderCompletion, cancelOrder } = require('../controllers/confirmation.controller');
 const router = express.Router();
 
 router.post('/confirm', confirmOrder);
 router.post('/complete', confirmOrderCompletion);
 router.post('/register', confirmRegistration);
+router.post('/cancel', cancelOrder);
 
 module.exports = router;
