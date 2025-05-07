e4fc0eac6a71120770764c37de39c349db3503d1
Inupa Udara <127815279+inupaUdara@users.noreply.github.com>
2025-05-07 22:27:24 +0530

    Remove unused helper components and services to streamline the codebase

--- client/Website/src/helper/Auth.jsx
+++
@@ -1,8 +0,0 @@
-export const isAuthenticated = () => {
-    return !!localStorage.getItem('token');
-  };
-  
-  export const logout = () => {
-    localStorage.removeItem('token');
-    window.location.href = '/login';
-  };
